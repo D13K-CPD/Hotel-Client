5b73d5da29bc720c109998d963b18fbd62318538
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-03-22 20:00:10 +0530

    Create main-header component
    Create main-header component

---
+++ src/app/inner-items/main-header/main-header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-main-header',
+  imports: [MatIcon],
+  templateUrl: './main-header.html',
+  styleUrl: './main-header.scss',
+})
+export class MainHeader {}
