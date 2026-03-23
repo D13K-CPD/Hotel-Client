3773149c815a12c03b32fce85b10df3e9bd55644
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-03-23 21:55:00 +0530

    Create Activities Context Component
    Create Activities Context Component

---
+++ src/app/inner-items/activities-context-component/activities-context-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activities-context-component',
+  imports: [],
+  templateUrl: './activities-context-component.html',
+  styleUrl: './activities-context-component.scss',
+})
+export class ActivitiesContextComponent {}
