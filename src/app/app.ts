471a48c93363ab26eb7c9434565866b1d202368e
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-03-30 07:43:13 +0530

    update MainHeader location
    update MainHeader location

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { MainHeader } from './components/main-header/main-header';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer-component/footer-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent],
+  imports: [MainHeader, RouterOutlet, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
