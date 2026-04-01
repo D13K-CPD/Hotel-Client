43c839e63a5f99100c83afa1f0473a82729c0c62
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-04-01 20:26:44 +0530

    Update login
    Update login

--- src/app/pages/security/login/login.ts
+++ src/app/pages/security/login/login.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatFormField, MatLabel, MatFormFieldModule } from '@angular/material/form-field';
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-login',
-  imports: [MatFormField, MatFormFieldModule],
+  imports: [MatFormField, MatFormFieldModule, MatAnchor],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
