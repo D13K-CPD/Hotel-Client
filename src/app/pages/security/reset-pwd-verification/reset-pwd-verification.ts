36d33c7e7fea094adcf1c9c22558cb00a43f3e05
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-04-02 18:19:48 +0530

    Create ResetPwdVerification section
    Create ResetPwdVerification section

---
+++ src/app/pages/security/reset-pwd-verification/reset-pwd-verification.ts
@@ -0,0 +1,13 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MatAnchor } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { MatFormField, MatLabel, MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-reset-pwd-verification',
+  imports: [MatAnchor, FormsModule, RouterLink, MatFormField, MatLabel, MatFormFieldModule],
+  templateUrl: './reset-pwd-verification.html',
+  styleUrl: './reset-pwd-verification.scss',
+})
+export class ResetPwdVerification {}
