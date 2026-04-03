51e11bd25c2a74af5922e8cb34aefc2a475a82c3
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-04-03 19:23:38 +0530

    MatInputModule error fix in forgot-password, register-verification and reset-password
    MatInputModule error fix in forgot-password, register-verification and reset-password

--- src/app/pages/security/forgot-password/forgot-password.ts
+++ src/app/pages/security/forgot-password/forgot-password.ts
@@ -3,10 +3,19 @@ import { MatAnchor } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MatFormField, MatLabel, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [MatAnchor, FormsModule, RouterLink, MatFormField, MatLabel, MatFormFieldModule],
+  imports: [
+    MatAnchor,
+    FormsModule,
+    RouterLink,
+    MatFormField,
+    MatLabel,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.scss',
 })
