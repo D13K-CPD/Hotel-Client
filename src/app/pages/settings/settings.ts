72bee2e6b630941a5345ed92dc6b0c464aad3a21
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-04-05 07:51:50 +0530

    Remove unused imports
    Remove unused imports

--- src/app/pages/settings/settings.ts
+++ src/app/pages/settings/settings.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { ProfileSettings } from './inner/profile-settings/profile-settings';
 import { History } from './inner/history/history';
-import { MatIcon } from '@angular/material/icon';
-import { StaysContextComponent } from '../../inner-items/options-component/stays-context-component/stays-context-component';
 
 @Component({
   selector: 'app-settings',
-  imports: [MatTabsModule, ProfileSettings, History, MatIcon, StaysContextComponent],
+  imports: [MatTabsModule, ProfileSettings, History],
   templateUrl: './settings.html',
   styleUrl: './settings.scss',
 })
