431730cb764d01a849302788a35a70bc998ca27b
Ranidu Nethma <113999336+RaniduNethma@users.noreply.github.com>
2026-04-05 07:43:55 +0530

    Update hotel-card component
    Update hotel-card component

--- src/app/pages/search-result/inner/hotel-card/hotel-card.ts
+++ src/app/pages/search-result/inner/hotel-card/hotel-card.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MapView } from '../map-view/map-view';
 
 @Component({
   selector: 'app-hotel-card',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './hotel-card.html',
   styleUrl: './hotel-card.scss',
 })
-export class HotelCard {}
+export class HotelCard {
+  dialog = inject(MatDialog);
+
+  openDialog() {
+    this.dialog.open(MapView, {
+      width: '1000px',
+      height: '600px',
+      data: {},
+      disableClose: true,
+    });
+  }
+}
